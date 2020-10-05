5a20e683da4b40870e54689a74de5a39344a52c0
RyumaNoma <rskm.2002722@gmail.com>
2020-10-05 20:09:28 +0900

    Main関数をRandomAIに変更

--- ConsoleOthello/Main.cpp
+++ ConsoleOthello/Main.cpp
@@ -1,13 +1,13 @@
-﻿#include "D:\VisualOthello\VisualOthello\Game.h"
-#include "D:\VisualOthello\VisualOthello\PersonPlayer.h"
-#include "D:\VisualOthello\VisualOthello\AIPlayer.h"
+﻿#include "..\VisualOthello\Game.h"
+#include "..\VisualOthello\PersonPlayer.h"
+#include "..\VisualOthello\RandomAI.h"
 
 // TODO: リプレイ機能の追加
 
 int main(void)
 {
 	PersonPlayer player1;
-	AIPlayer player2;
+	RandomAI player2;
 	Game game(&player1, &player2);
 
 	game.start();
